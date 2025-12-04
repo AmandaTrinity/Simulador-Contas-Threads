829ae0af7c1ece8409e862fdd931c46f668a5f81
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-03 23:12:17 -0300

    feat: adiciona prótotipo das funçĩes de bankAccount

--- bankAccount.h
+++ bankAccount.h
@@ -5,60 +5,27 @@
 #include <pthread.h>
 #include <stdio.h>
 
-#define NUM_ACCOUNTS 5 //numero de conta
+#define NUM_ACCOUNTS 5
 
-// Shared Variables --> array de mutexes, é um para cada conta
-pthread_mutex_t locks[NUM_ACCOUNTS] = {PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER
-                                       ,PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER}; //variavel de exclusao mutua inicializada
+//Variáveis Compartilhadas
+//extern--> 'esta variável existe, mas será definida em outro lugar[.c]'
 
-//Array de saldos, um para cada conta                                       
-double bankAccountBalances[NUM_ACCOUNTS] = {0.0};//guarda o saldo da conta --> é o nosso recurso compartilhado
+//Array de mutexes para exclusão mútua local por conta
+extern pthread_mutex_t locks[NUM_ACCOUNTS]; 
 
-//Adiciona um valor ao saldo de uma conta específica
-void deposit(int accountID, double amount) {
-   if(accountID >= 0 && accountID < NUM_ACCOUNTS) {
-      pthread_mutex_lock(&locks[accountID]);
-      bankAccountBalances[accountID] += amount;
-      pthread_mutex_unlock(&locks[accountID]);
-   }
-}
+//Array de saldos
+extern double bankAccountBalances[NUM_ACCOUNTS]; 
+
+//Protótipos de Funções
+//adiciona um valor ao saldo de uma conta específica
+void deposit(int accountID, double amount);
 
 //Subtrai um valor do saldo de uma conta específica
 //Retorna 1 em sucesso, 0 em falha (saldo insuficiente), -1 para conta inválida
-int withdraw(int accountID, double amount) {
-   int success = 0; //assume falha por padrão
-
-   if(accountID >= 0 && accountID < NUM_ACCOUNTS) {
-      pthread_mutex_lock(&locks[accountID]);
- 
-      if(bankAccountBalances[accountID] >= amount){
-         bankAccountBalances[accountID] -= amount;
-         success = 1;
-      
-      }
-      pthread_mutex_unlock(&locks[accountID]);
-   } else {
-      success = -1; //Conta Inválida
-   }
-
-   return success;
-}
+int withdraw(int accountID, double amount);
 
 //Consulta de Saldo com exclusao mútua
-double checkBalance(int accountID) {
-   double balance = -1.0;
-   if(accountID >= 0 && accountID < NUM_ACCOUNTS) {
-      pthread_mutex_lock(&locks[accountID]);
-      balance = bankAccountBalances[accountID];
-      pthread_mutex_unlock(&locks[accountID]);
-   }
-   return balance;
-}
+double checkBalance(int accountID);
 
 //Destruir o mutex(cadeado) para liberar os recursos do sistema
-void cleanup() {
-   int i;
-   for(i = 0; i < NUM_ACCOUNTS; i++) {
-      pthread_mutex_destroy(&locks[i]);
-   }
-}
+void cleanup();

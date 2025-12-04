21802448ebea1618657e849ad1c6bd998bd0a3b7
Amanda Trinity <amandatrinity16@gmail.com>
2025-12-03 22:41:29 -0300

    feature: complementa a função deposit

--- bankAccount.h
+++ bankAccount.h
@@ -4,15 +4,26 @@
  */
 
 
-// Shared Variables
-pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-double bankAccountBalance = 0;
 
-// add amount to bankAccountBalance
-void deposit(double amount) {
-   //pthread_mutex_lock( &lock );
-   bankAccountBalance += amount;
-  // pthread_mutex_unlock( &lock );
+#include <pthread.h>
+#include <stdio.h>
+
+#define NUM_ACCOUNTS 5 //numero de conta
+
+// Shared Variables --> array de mutexes, é um para cada conta
+pthread_mutex_t locks[NUM_ACCOUNTS] = {PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER
+                                       ,PTHREAD_MUTEX_INITIALIZER,PTHREAD_MUTEX_INITIALIZER}; //variavel de exclusao mutua inicializada
+
+//Array de saldos, um para cada conta                                       
+double bankAccountBalances[NUM_ACCOUNTS] = {0.0};//guarda o saldo da conta --> é o nosso recurso compartilhado
+
+//Adiciona um valor ao saldo de uma conta específica
+void deposit(int accountID, double amount) {
+   if(accountID >= 0 && accountID < NUM_ACCOUNTS) {
+      pthread_mutex_lock(&locks[accountID]);
+      bankAccountBalances[accountID] += amount;
+      pthread_mutex_unlock(&locks[accountID]);
+   }
 }
 
 // subtract amount from bankAccountBalance
